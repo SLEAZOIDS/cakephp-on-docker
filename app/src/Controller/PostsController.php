467240a8fafa822a14d16ead402d163b64e2de5d
SLEAZOIDS <vegetarian304@gmail.com>
2018-01-10 23:23:27 +0900

    posts MVC

---
+++ app/src/Controller/PostsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+class PostsController extends AppController
+{
+    public function index()
+    {
+        $posts =$this->Posts->find('all');
+        // $this->set('posts', $posts);
+        $this->set(compact('posts'));
+    }
+}
---
+++ app/src/Template/Posts/index.ctp
@@ -0,0 +1,7 @@
+<h1>Blog Post</h1>
+
+<ul>
+    <?php foreach ($posts as $post) : ?>
+        <li><?= h($post->title); ?></li>
+    <?php endforeach; ?>
+</ul>
